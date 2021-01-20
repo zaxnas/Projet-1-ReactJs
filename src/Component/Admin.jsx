10b28744e38304a8cff3a420a730042f44fbb9bf
yoamb97@gmail.com <yoamb97@gmail.com>
2021-01-20 20:00:34 +0200

    modification projet'

--- src/Component/Admin.jsx
+++ src/Component/Admin.jsx
@@ -68,99 +68,132 @@ class Admin extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>Admin</h1> 
-                <h1>movie</h1> 
-                <input
-                    type="text"
-                    onChange ={e => this.setState({image:e.target.value})}
-                    ref = 'imageInputMovie'
-                    placeholder = "Enter your Picture ..." />
-                <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({title:e.target.value})}
-                    ref = 'titleInputMovie'
-                    placeholder = "Enter your Title of Movie ..."/>
-                    <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({average:e.target.value})}
-                    ref = 'averageInputMovie'
-                    placeholder = "Enter The Avecrage ..."/>
-                <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({price:e.target.value})}
-                    ref = 'priceInputMovie'
-                    placeholder = "Enter the Price ..."/>
-                <br/><br/>      
-                <input 
-                    type="button"     
-                    value = "Update"
-                    onClick = {this.UpdateMovie}/> 
-               <br/><br/> <br/><br/>  
-               <h1>serie</h1>   
-               <input
-                    type="text"
-                    onChange ={e => this.setState({image:e.target.value })}
-                    ref = 'imageInputSerie'
-                    placeholder = "Enter your Picture ..." />
-                <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({title:e.target.value})}
-                    ref = 'titleInputSerie'
-                    placeholder = "Enter your Title of Movie ..."/>
-                    <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({average:e.target.value})}
-                    ref = 'averageInputSerie'
-                    placeholder = "Enter The Avecrage ..."/>
-                <br/><br/>   
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({price:e.target.value})}
-                    ref = 'priceInputSerie'
-                    placeholder = "Enter the Price ..."/>
-                <br/><br/>      
-                <input 
-                    type="button"     
-                    value = "Update"
-                    onClick = {this.UpdateSerie}/> 
-               <br/><br/> <br/><br/>  
-               <h1>anime</h1>         
-               <input
-                    type="text"
-                    onChange ={e => this.setState({image:e.target.value})}
-                    ref = 'imageInputAnime'
-                    placeholder = "Enter your Picture ..." />
-                <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({title:e.target.value})}
-                    ref = 'titleInputAnime'
-                    placeholder = "Enter your Title of Movie ..."/>
-                    <br/><br/>
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({average:e.target.value})}
-                    ref = 'averageInputAnime'
-                    placeholder = "Enter The Avecrage ..."/>
-                <br/><br/>   
-                <input 
-                    type="text"
-                    onChange = {e => this.setState({price:e.target.value})}
-                    ref = 'priceInputAnime'
-                    placeholder = "Enter the Price ..."/>
-                <br/><br/>      
-                <input 
-                    type="button"     
-                    value = "Update"
-                    onClick = {this.UpdateAnime}/> 
-               <br/><br/> <br/><br/>    
-               <Link to = "/movie">movie</Link>            
+            <div className = "container-fluid" style={{height:'100vh'}}>
+                <div className = "row" style = {{}}>
+                    <div style={{flex:1,flexDirection:'column',display:'flex',alignContent:'center',flexWrap:"wrap",margin:'0px 7em',marginTop:'20vh'}} > 
+                        <h3 style= {{fontFamily:'Lobster',fontSize:'40px' ,color:'white'}}><strong><i>Add Movie</i></strong></h3> 
+                        <input
+                            type="text"
+                            onChange ={e => this.setState({image:e.target.value})}
+                            ref = 'imageInputMovie'
+                            placeholder = "Enter your Picture ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>
+                        <input 
+                            type="text"
+                            onChange = {e => this.setState({title:e.target.value})}
+                            ref = 'titleInputMovie'
+                            placeholder = "Enter your Title of Movie ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                            <br/><br/>
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({average:e.target.value})}
+                            ref = 'averageInputMovie'
+                            placeholder = "Enter The Average ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({price:e.target.value})}
+                            ref = 'priceInputMovie'
+                            placeholder = "Enter the Price ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>      
+                        <input 
+                            type="button"     
+                            value = "Update"
+                            onClick = {this.UpdateMovie}
+                            className = "btn btn-warning"/> 
+                    </div>
+                <div style={{flex:1,flexDirection:'column',display:'flex',alignContent:'center',flexWrap:"wrap",margin:'0px 7em',marginTop:'20vh'}}> 
+                        <h3 style= {{fontFamily:'Lobster',fontSize:'40px' ,color:'white'}}><strong><i>Add Serie</i></strong></h3>    
+                        <input
+                            type="text"
+                            onChange ={e => this.setState({image:e.target.value })}
+                            ref = 'imageInputSerie'
+                            placeholder = "Enter your Picture ..." 
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>
+                        <input 
+                            type="text"
+                            onChange = {e => this.setState({title:e.target.value})}
+                            ref = 'titleInputSerie'
+                            placeholder = "Enter your Title of Serie ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                            <br/><br/>
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({average:e.target.value})}
+                            ref = 'averageInputSerie'
+                            placeholder = "Enter The Average ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>   
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({price:e.target.value})}
+                            ref = 'priceInputSerie'
+                            placeholder = "Enter the Price ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>      
+                        <input 
+                            type="button"     
+                            value = "Update"
+                            onClick = {this.UpdateSerie}
+                            className = "btn btn-warning"/> 
+                    </div>
+                    <div style={{flex:1,flexDirection:'column',display:'flex',alignContent:'center',flexWrap:"wrap",margin:'0px 7em',marginTop:'20vh'}}>
+                    <h3 style= {{fontFamily:'Lobster',fontSize:'40px' ,color:'white'}}><strong><i>Add Anime</i></strong></h3>          
+                    <input
+                            type="text"
+                            onChange ={e => this.setState({image:e.target.value})}
+                            ref = 'imageInputAnime'
+                            placeholder = "Enter your Picture ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}}  />
+                        <br/><br/>
+                        <input 
+                            type="text"
+                            onChange = {e => this.setState({title:e.target.value})}
+                            ref = 'titleInputAnime'
+                            placeholder = "Enter your Title of Anime ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                            <br/><br/>
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({average:e.target.value})}
+                            ref = 'averageInputAnime'
+                            placeholder = "Enter The Average ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>   
+                        <input 
+                            type="number"
+                            onChange = {e => this.setState({price:e.target.value})}
+                            ref = 'priceInputAnime'
+                            placeholder = "Enter the Price ..."
+                            className = "form-control" 
+                            style = {{width:'300px' ,border:'solid 2px black'}} />
+                        <br/><br/>      
+                        <input 
+                            type="button"     
+                            value = "Update"
+                            onClick = {this.UpdateAnime}
+                            className = "btn btn-warning"/> 
+                    </div> 
+                </div>
+                <br/><br/><br/>
+                <div className='container'><Link className = "btn btn-primary  btn-block"  to = "/">Login</Link></div>
+                         
             </div>
         )
     }
--- src/Component/DisgnPage.jsx
+++ src/Component/DisgnPage.jsx
@@ -4,15 +4,17 @@ import './DisgnPage.css'
 export default function DisgnPage(props) {
 
     const SetVoteClass = (vote) => {
-        if( vote >= 8){
+        if( vote >= 8 && vote <= 10){
             return "green"
         }
-        else if(vote >= 5){
+        else if(vote >= 5 && vote <= 7){
             return "orange"
         }
-        else{
+        else if (vote >= 0 && vote <= 4){
             return "red"
         }
+        
+
     }
     
     /*function a(){
@@ -22,7 +24,7 @@ export default function DisgnPage(props) {
         <div className ="" style = {{padding:'5px',position:"relative"}}>
                 <img className = "imgMSA" src={props.msa.image}  alt="img" />
                 <h5>{props.msa.title}</h5>
-                <h5 className="heading-4">{props.msa.price}$ <span className = {` tag ${SetVoteClass(props.msa.average)}`} style={{marginLeft:'10px',borderRadius:'5px'}} ><strong>{props.msa.average}</strong></span></h5> 
+                <h5 className="heading-4">{props.msa.price}$ <span className = {` tag ${SetVoteClass(props.msa.average)}`} style={{marginLeft:'10px',borderRadius:'5px'}} ><strong>{props.msa.average > 11 || props.msa.average < 0 ? 'N.A' : props.msa.average }</strong></span></h5> 
                 { <input type="button"
                  value="Buy Now !" 
                  onClick={()=>{props.GetBuy(props.msa )}}
